40d77f8a9f708735875547d4799b7e60f08fe3d8
Nico Buldo <sombenicolas@gmail.com>
2025-09-08 17:57:43 +0200

    Commit #3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 
 console.log('TP CIEL');
 
@@ -22,6 +22,75 @@ exp.use(function (err, req, res, next) {
     res.status(500).send('Erreur serveur express');
 });
 
-exp.listen(80, function () {
+
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
+}); 
+
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
     console.log('Serveur en ecoute');
-});
+}); 
+
+window.onload = function () {
+    if (TesterLaCompatibilite()) {
+        ConnexionAuServeurWebsocket();
+    }
+    ControleIHM();
+}
+
+function TesterLaCompatibilite() {
+    let estCompatible = true;
+    if (!('WebSocket' in window)) {
+        window.alert('WebSocket non supporté par le navigateur');
+        estCompatible = false;
+    }
+    return estCompatible;
+
+    /*  ***************** Connexion au serveur WebSocket ********************   */
+    // 
+    function ConnexionAuServeurWebsocket() {
+        ws = new WebSocket('ws://' + ipServeur + '/echo');
+
+        ws.onclose = function (evt) {
+            window.alert('WebSocket close');
+        };
+
+        ws.onopen = function () {
+            console.log('WebSocket open');
+        };
+
+        ws.onmessage = function (evt) {
+            document.getElementById('messageRecu').value = evt.data;
+        };
+    }
+
+    function ControleIHM() {
+        document.getElementById('Envoyer').onclick = BPEnvoyer;
+    }
+
+    function BPEnvoyer() {
+        ws.send(document.getElementById('messageEnvoi').value)
+    };
+};
---
+++ www/js/echo.js
@@ -0,0 +1,2 @@
+var ipServeur = '172.17.50.xxx';     // Adresse ip du serveur  
+var ws;                             // Variable pour l'instance de la WebSocket. 
