9fdd77deaea86205e227743be6b7173a38c45571
Nico Buldo <sombenicolas@gmail.com>
2025-09-08 16:44:16 +0200

    Commit #2

--- app.js
+++ app.js
@@ -2,3 +2,26 @@
 
 console.log('TP CIEL');
 
+/*  *********************** Serveur Web ***************************   */
+// 
+
+var express = require('express'); 
+
+
+var exp = express(); 
+
+exp.use(express.static(__dirname + '/www')); 
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+exp.listen(80, function () {
+    console.log('Serveur en ecoute');
+});
